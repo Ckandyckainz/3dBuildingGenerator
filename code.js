43423cada9667a5c6690ef9dd0d8e6ee2c7f0b6d
Ckandyckainz <74159422+Ckandyckainz@users.noreply.github.com>
2022-03-20 10:34:57 -0700

    basic stuff
    created index.html and code.js, and made basic code for maincanvas or mcan

---
+++ code.js
@@ -0,0 +1,6 @@
+let mcan = document.getElementById("maincanvas");
+let mctx = mcan.getContext("2d");
+mcan.width = window.innerWidth;
+mcan.height = window.innerHeight;
+let mcw = mcan.width;
+let mch = mcan.height;
